195bd077de5806fb1cc34edcd3f0d44a949784f1
Tsah Elbaz <tsah.elbaz@gmail.com>
2026-08-04 21:54:06 +0300

    integration player and grid

--- Assets/Art/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
+++ Assets/Art/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -1,50 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using AnalogOverride.GridSystem; 
 namespace Cainos.PixelArtTopDown_Basic
 {
-    public class TopDownCharacterController : MonoBehaviour
+    public class TopDownCharacterController : GridEntity
     {
-        public float speed;
-
         private Animator animator;
 
-        private void Start()
+        protected override void Start()
         {
+            base.Start(); // Snaps the player to the grid's center on spawn
             animator = GetComponent<Animator>();
         }
 
-
         private void Update()
         {
-            Vector2 dir = Vector2.zero;
+            // If the visual slide from the last step is still playing, ignore new input
+            if (IsMoving)
+            {
+                animator.SetBool("IsMoving", true);
+                return;
+            }
+
+            Vector2Int dir = Vector2Int.zero;
+
+            // Determine discrete grid direction based on input
             if (Input.GetKey(KeyCode.A))
             {
-                dir.x = -1;
+                dir = Vector2Int.left;
                 animator.SetInteger("Direction", 3);
             }
             else if (Input.GetKey(KeyCode.D))
             {
-                dir.x = 1;
+                dir = Vector2Int.right;
                 animator.SetInteger("Direction", 2);
             }
-
-            if (Input.GetKey(KeyCode.W))
+            else if (Input.GetKey(KeyCode.W))
             {
-                dir.y = 1;
+                dir = Vector2Int.up;
                 animator.SetInteger("Direction", 1);
             }
             else if (Input.GetKey(KeyCode.S))
             {
-                dir.y = -1;
+                dir = Vector2Int.down;
                 animator.SetInteger("Direction", 0);
             }
 
-            dir.Normalize();
-            animator.SetBool("IsMoving", dir.magnitude > 0);
+            // If a directional key is pressed, attempt to step on the grid
+            if (dir != Vector2Int.zero)
+            {
+                // TryStep handles checking for walls, pushing blocks, and starting the movement coroutine
+                TryStep(dir);
+            }
 
-            GetComponent<Rigidbody2D>().linearVelocity = speed * dir;
+            // Update the animator based on whether a valid move was accepted
+            animator.SetBool("IsMoving", IsMoving);
         }
     }
 }
